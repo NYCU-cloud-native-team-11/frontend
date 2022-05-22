74bdfea692b3741842ed5e74115684c0e8695535
hsinlunlun <hsinlun.chiu@gmail.com>
2022-05-23 01:59:26 +0800

    initialization for react and chart.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import * as React from 'react';
+import { Container } from '@mui/material';
+import { Box } from '@mui/system';
+import { AppBar } from '@mui/material'
+import { Toolbar}  from '@mui/material';
+import { Typography } from '@mui/material';
+import { UserData } from "./Data";
+import LineChart from "./components/LineChart";
+
 
 function App() {
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [
+      {
+        label: "Count",
+        data: UserData.map((data) => data.count),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+        ],
+        borderColor: "rgba(75,192,192,1)",
+        borderWidth: 2,
+      },
+    ],
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Box>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              NYCU Cloud Native Development
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Container maxWidth="md">
+          <Box marginTop={3} >
+            <LineChart chartData={userData} />
+          </Box>
+        </Container>
+      </Box>
   );
 }
 
---
+++ src/Data.js
@@ -0,0 +1,32 @@
+export const UserData = [
+  {
+    id: 1,
+    year: 2016,
+    company: "TSMC",
+    count: 823,
+  },
+  {
+    id: 2,
+    year: 2017,
+    company: "TSMC",
+    count: 345,
+  },
+  {
+    id: 3,
+    year: 2018,
+    company: "TSMC",
+    count: 555,
+  },
+  {
+    id: 4,
+    year: 2019,
+    company: "TSMC",
+    count: 4555,
+  },
+  {
+    id: 5,
+    year: 2020,
+    company: "TSMC",
+    count: 234,
+  },
+];
