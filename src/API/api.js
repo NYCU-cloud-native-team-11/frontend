dd7c9c6310ca435fccf669d9d21db182b51addcd
hsinlunlun <hsinlun.chiu@gmail.com>
2022-06-05 22:38:21 +0800

    env test

--- src/API/api.js
+++ src/API/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 // require('dotenv');
 
+console.log("process.env.REACT_APP_BASE_URL", process.env.REACT_APP_BASE_URL);
+
 export default axios.create({
   baseURL: process.env.REACT_APP_BASE_URL
 });
